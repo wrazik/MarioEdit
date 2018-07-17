f7d230f5abf776b85835901a9e9d4d9a1de32ac5
Marcin Kukliński <senghe@gmail.com>
2018-07-17 19:50:42 +0200

    fix Axis tests

--- src/tests/Axis.cpp
+++ src/tests/Axis.cpp
@@ -3,14 +3,16 @@
 #include "classes/Axis.hpp"
 
 TEST(AxisTest, test_calculation_on_x_axis) {
-    Axis::rescale({200, 300});
+    sf::Vector2u newSize(200, 300);
+    Axis::rescale(newSize);
 
     Axis calc;
     ASSERT_EQ(44, calc.percent(22, Direction::X));
 }
 
 TEST(AxisTest, test_calculation_on_y_axis) {
-    Axis::rescale({200, 300});
+    sf::Vector2u newSize(200, 300);
+    Axis::rescale(newSize);
 
     Axis calc;
     ASSERT_EQ(66, calc.percent(22, Direction::Y));
