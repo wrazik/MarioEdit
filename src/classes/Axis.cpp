f7ca557d047302f18665085280dfaab38e4815e8
Marcin Kukliński <senghe@gmail.com>
2018-07-21 15:01:49 +0200

    Remove unused code

--- src/classes/Axis.cpp
+++ src/classes/Axis.cpp
@@ -8,16 +8,14 @@
 float Axis::scale = 1;
 
 void Axis::rescale(sf::Vector2u windowSize) {
-    this->windowSize = windowSize;
+    float prevScale = Axis::scale;
+    scale = (float)windowSize.y / (float)(blockHeight*blocksOnHeight);
 
-    float prevScale = this->scale;
-    this->scale = (float)windowSize.y / (float)(this->blockHeight*this->blocksOnHeight);
-
-    if (prevScale == this->scale) {
+    if (prevScale == Axis::scale) {
         return;
     }
 
-    float scaleRatio = this->scale/prevScale;
+    float scaleRatio = Axis::scale/prevScale;
 
     auto tiles = TileRegistry::getAll();
     for (int i=0; i<tiles.size(); i++) {
@@ -25,16 +23,6 @@ void Axis::rescale(sf::Vector2u windowSize) {
         auto position = tile->getPosition();
         tile->setPosition(position.x*scaleRatio, position.y*scaleRatio);
     }
-}
-
-std::size_t Axis::percent(float percent, Direction axis) {
-
-    size_t calcFrom = this->windowSize.y;
-    if (axis == Direction::X) {
-        calcFrom = this->windowSize.x;
-    }
-
-    return (percent/100)*calcFrom;
 }
 
 float Axis::getScale() {
--- src/classes/Axis.hpp
+++ src/classes/Axis.hpp
@@ -12,12 +12,10 @@ public:
 
     void rescale(sf::Vector2u windowSize);
 
-    std::size_t percent(float percent, Direction axis);
     static float getScale();
 
 private:
 
-    sf::Vector2u windowSize = {0, 0};
     const std::size_t blocksOnHeight = 12;
     const std::size_t blockHeight = 16;
     static float scale;
